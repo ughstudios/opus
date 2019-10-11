4a80cb6c3835bad36cc74dcf6021a8e40ecc87c0
TimFaletti <tim.faletti@gmail.com>
2019-10-11 00:28:20 -0400

    Basic Day/Night cycle and sun

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public static DayNightCycle cycle = null;
+
+    public float dayLength = 600f;
+    public float dayTimeProportion = 0.5f;
+
+    public float time = 0f;
+    public int numDays = 0;
+
+    void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        if (time > dayLength)
+        {
+            time -= dayLength;
+            numDays++;
+        }
+    }
+
+    public bool IsDayTime()
+    {
+        float dayStart = dayLength / 2f - dayLength * dayTimeProportion / 2f;
+        //float dayEnd = dayLength / 2f + dayLength * dayTimeProportion / 2f;
+        float dayEnd = dayLength - dayStart;
+        return time > dayStart && time < dayEnd;
+    }
+
+    public bool IsNightTime()
+    {
+        return !IsDayTime();
+    }
+
+    private void OnDisable()
+    {
+        cycle = null;
+    }
+
+    private void OnEnable()
+    {
+        if (cycle != null)
+        {
+            Debug.LogWarningFormat("{0} OnEnable: cycle == {1} ; Overriding", this, cycle);
+        }
+        cycle = this;
+    }
+}
---
+++ Assets/Scripts/SunController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Light))]
+public class SunController : MonoBehaviour
+{
+    new private Light light;
+
+    public Vector3 rotAxis = Vector3.right;
+    public Vector3 baseRot = new Vector3(0f, 90f, 0f);
+
+    public Color normal = Color.white;
+    public Color twilight = Color.red;
+
+    public float twilightProportion = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (DayNightCycle.cycle == null)
+            return;
+
+        float time = DayNightCycle.cycle.time;
+        float dayLength = DayNightCycle.cycle.dayLength;
+        float noon = dayLength / 2f;
+        float dayTimeProportion = DayNightCycle.cycle.dayTimeProportion;
+        float dayStart = (dayLength - dayLength * dayTimeProportion) / 2f;
+        float dayEnd = dayLength - dayStart;
+
+        float angle = 0f;
+
+        if (time > dayStart && time < dayEnd)
+        {
+            angle = 180f * ((dayEnd - time) / (dayEnd - dayStart));
+        }
+        else if (time < dayStart)
+        {
+            angle = 180f + 90f * ((dayStart - time) / dayStart);
+        }
+        else
+        {
+            angle = -90f * ((time - dayEnd) / (dayLength - dayEnd));
+        }
+
+        Vector3 orientation = baseRot + rotAxis * angle;
+
+        transform.eulerAngles = orientation;
+
+        //TODO: Twilight
+    }
+}
