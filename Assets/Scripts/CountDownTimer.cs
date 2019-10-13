e8ac33983f1e375dfad271b09e3f0c5eb1f6bb52
jonnubroth <jonnubroth@hotmail.com>
2019-10-13 18:34:14 -0400

    water/food HUD
    Created the HUD for food and water. Timer can be changed depending on difficulty wanted. Working on creating random drops of food and water to add time back to sliders.

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 100f;
+
+    public Slider waterBar;
+    public Slider foodBar;
+    
+
+    void Start()
+    {
+        currentTime = startingTime;
+
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        
+
+        waterBar.value = currentTime;
+        foodBar.value = currentTime;
+    }
+}
