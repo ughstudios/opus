4ad79fa7f4bf885b2192355165b84df5af2e0e69
TimFaletti <tim.faletti@gmail.com>
2019-10-23 14:06:00 -0400

    Revert "Revert "added ui for inventory system.""
    This reverts commit e402d661d3fb87a383e16dbcca5f8247ab5320dd.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -10,11 +11,64 @@ public class Inventory : MonoBehaviour
 
     public GameObject Inventory_ScrolLView;
     public GameObject UI_Item_Prefab;
+    public GameObject Pickup_UI;
     public GameObject Inventory_UI;
+    public GameObject InventoryPanel_UI;
+
+    public GameObject UI_InventoryItemPrefab;
+
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+        
+    private void Update()
+    {
+        if (Input.GetButtonDown("OpenInventory"))
+        {
+            Inventory_UI.SetActive(!Inventory_UI.activeSelf);
+            if (Inventory_UI.activeSelf)
+            {
+                GenerateInventoryUI_Items();
+            }
+        }
+    }
+
+    private void GenerateInventoryUI_Items()
+    {
+        // Destroy all child inventory item widgets.
+        foreach (Transform child in InventoryPanel_UI.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (ItemData item in InventoryItems)
+        {
+            GameObject go = Instantiate(UI_InventoryItemPrefab);
+
+            Image image = go.GetComponent<Image>();
+            image.sprite = item.itemIcon;
+
+            TextMeshProUGUI text = go.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = "x" + item.itemCount;
+            go.transform.SetParent(InventoryPanel_UI.transform);
+
+            Button button = go.GetComponent<Button>();
+            button.onClick.AddListener(ItemClicked);
+        }
+
+    }
+
+    private void ItemClicked()
+    {
+        
+    }
 
     public void AddItemToInventory(ItemData item)
     {
-        Inventory_UI.SetActive(true);
+        Pickup_UI.SetActive(true);
 
         ItemData theFoundItem = new ItemData();
         bool itemFound = GetItemByItemName(item.itemName, ref theFoundItem);
@@ -41,9 +95,8 @@ public class Inventory : MonoBehaviour
     public void RemoveCountOfItemFromInventory(int removecount, ItemData item)
     {
 
-        Inventory_UI.SetActive(false);
-
-
+        Pickup_UI.SetActive(false);
+        
         ItemData theFoundItem = new ItemData();
         bool itemFound = GetItemByItemName(item.itemName, ref theFoundItem);
         if (itemFound)
@@ -91,7 +144,7 @@ public class Inventory : MonoBehaviour
 
         yield return new WaitForSeconds(5);
 
-        Inventory_UI.SetActive(false);
+        Pickup_UI.SetActive(false);
         Destroy(ui_item);
     }
 
