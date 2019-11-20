4657cf313640b6d0867616b17eaaf973c732d7d8
Daniel <damianscape@gmail.com>
2019-11-20 14:09:25 -0500

    Trees and rocks are mostly harvestable by using the (E) key. You can now click items in their inventory and they will call the ItemClicked() function in inventory.cs

--- Assets/Scripts/Harvestable.cs
+++ Assets/Scripts/Harvestable.cs
@@ -8,7 +8,7 @@ public class Harvestable : MonoBehaviour
     public float ResourceTimerSeconds = 60;
     [SerializeField]
     public ItemData item = new ItemData();
-    public bool bHarvested = false;
+    private bool bHarvested = false;
 
     void Start()
     {
@@ -17,6 +17,11 @@ public class Harvestable : MonoBehaviour
 
     void OnTriggerStay(Collider col)
     {
+        if (bHarvested)
+        {
+            return;
+        }
+
         if (col.gameObject.tag == "Player")
         {
             if (Input.GetButtonDown("Gather"))
@@ -34,9 +39,20 @@ public class Harvestable : MonoBehaviour
         {
             if (bHarvested)
             {
-                GetComponentsInChildren(MeshCollider)
+                Renderer[] rs = GetComponentsInChildren<Renderer>();
+                foreach (Renderer r in rs)
+                {
+                    r.enabled = false;
+                }
+
                 yield return new WaitForSeconds(ResourceTimerSeconds);
-                gameObject.SetActive(true);
+
+                foreach (Renderer r in rs)
+                {
+                    r.enabled = true;
+                }
+
+                bHarvested = false;
             }
 
             yield return new WaitForEndOfFrame();
